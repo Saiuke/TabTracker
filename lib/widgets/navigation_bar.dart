3914dec92891e9fa3d66537937bf8dfa69849841
Ezequias Lopes <ezequias@hotmail.com.br>
2024-05-15 21:05:14 -0500

    Created main menu

---
+++ lib/widgets/navigation_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MainBottomNavBar extends StatefulWidget {
+  const MainBottomNavBar({super.key});
+
+  @override
+  State<MainBottomNavBar> createState() => _MainBottomNavBarState();
+}
+
+class _MainBottomNavBarState extends State<MainBottomNavBar> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home'
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.messenger_sharp),
+            ),
+            label: 'Messages',
+          ),
+        ],
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
