3ba81931501a7369c083e0e2f54951ae482a6c17
Ezequias Lopes <ezequias@hotmail.com.br>
2024-05-18 17:43:23 -0500

    Add Customer model and CustomerCard widget.
    - Create a new file `customer_model.dart` with the `Customer` class.
    - Implement the `fromJson` and `toJson` methods in the `Customer` class.
    - Create a new file `customer_card.dart` with the `CustomerCard` widget.
    - Display customer information in a card format using the `CustomerCard` widget in the `CustomersPage`.
    - Populate the customer list with sample data.
    
    These changes add support for managing customer data and displaying it in a visually appealing way.

--- lib/pages/customers_page.dart
+++ lib/pages/customers_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/customer_model.dart';
+import '../widgets/customer_card.dart';
 import '../widgets/navigation_bar.dart';
 
 class CustomersPage extends StatefulWidget {
@@ -11,15 +13,75 @@ class CustomersPage extends StatefulWidget {
 }
 
 class _CustomersPageState extends State<CustomersPage> {
+  final customerList = [
+    Customer(
+        name: 'John Doe',
+        email: 'john-doe@example.com',
+        phone: '123-456-7890'
+    ),
+    Customer(
+        name: 'Jane Doe',
+        email: 'jane-doe@example.com',
+        phone: '098-765-4321'
+    ),
+    Customer(
+        name: 'Alana Castro',
+        email: 'alana-castro@example.com',
+        phone: '456-789-0123'),
+    Customer(
+        name: 'Michael Smith',
+        email: 'michael.smith@example.com',
+        phone: '321-654-0987'),
+    Customer(
+        name: 'Emily Johnson',
+        email: 'emily.johnson@example.com',
+        phone: '654-321-9876'),
+    Customer(
+        name: 'David Brown',
+        email: 'david.brown@example.com',
+        phone: '789-012-3456'),
+    Customer(
+        name: 'Olivia Williams',
+        email: 'olivia.williams@example.com',
+        phone: '012-345-6789'),
+    Customer(
+        name: 'James Miller',
+        email: 'james.miller@example.com',
+        phone: '234-567-8901'),
+    Customer(
+        name: 'Sophia Martinez',
+        email: 'sophia.martinez@example.com',
+        phone: '567-890-1234'),
+    Customer(
+        name: 'Isabella Anderson',
+        email: 'isabella.anderson@example.com',
+        phone: '890-123-4567'),
+    Customer(
+        name: 'Ethan Thomas',
+        email: 'ethan.thomas@example.com',
+        phone: '123-098-7654'),
+    Customer(
+        name: 'Charlotte Taylor',
+        email: 'charlotte.taylor@example.com',
+        phone: '345-678-9012'),
+    Customer(
+        name: 'Alexander Harris',
+        email: 'alexander.harris@example.com',
+        phone: '678-901-2345')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Customers'),
-        automaticallyImplyLeading: false,
-      ),
-      body: const Center(
-        child: Text('Customers Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: ListView.builder(
+          itemCount: customerList.length,
+          itemBuilder: (context, index) {
+            final currentCustomer = customerList[index];
+            return CustomerCard(customerInfo: currentCustomer);
+          },
+        ),
       ),
       bottomNavigationBar:
           MainBottomNavBar(currentPageIndex: widget.currentPageIndex),
